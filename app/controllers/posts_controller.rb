1bfb7db57ab273a7b503f834fb263452f01c4021
Jordan Lagan <jordanlagan000@gmail.com>
2017-10-16 15:44:38 -0400

    assignment 27: complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,8 @@ class PostsController < ApplicationController
 
   before_action :authorize_user, except: [:show, :new, :create]
 
+  before_action :authorize_delete, only: :destroy
+
   def show
     @post = Post.find(params[:id])
   end
@@ -62,11 +64,20 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :body)
   end
 
-   def authorize_user
+   def authorize_delete
      post = Post.find(params[:id])
      
      unless current_user == post.user || current_user.admin?
        flash[:alert] = "You must be an admin to do that."
+       redirect_to [post.topic, post]
+     end
+   end
+
+   def authorize_user
+     post = Post.find(params[:id])
+     
+     unless current_user == post.user || current_user.admin? || current_user.moderator?
+       flash[:alert] = "You must be an admin or mod to do that."
        redirect_to [post.topic, post]
      end
    end
