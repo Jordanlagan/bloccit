ad52cdd4beb7819281739f6f7fd516b01997fd29
Jordan Lagan <jordanlagan000@gmail.com>
2017-10-17 23:20:09 -0400

    assignment 30: complete

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -15,4 +15,16 @@ class FavoriteMailer < ApplicationMailer
 
      mail(to: user.email, subject: "New comment on #{post.title}")
    end
+
+   def new_post(post)
+     headers["Message-ID"] = "<comments/#{post.id}@bloccit.example>"
+     headers["In-Reply-To"] = "<post/#{post.id}@bloccit.example>"
+     headers["References"] = "<post/#{post.id}@bloccit.example>"
+
+     @post = post
+ 
+
+     mail(to: post.user.email, subject: "You're following your own post, #{post.title}")
+   end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -12,6 +12,8 @@ class Post < ApplicationRecord
 	validates :topic, presence: true
 	validates :user, presence: true
 
+  after_create :create_favorite
+
    def up_votes
      votes.where(value: 1).count
    end
@@ -29,4 +31,11 @@ class Post < ApplicationRecord
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
    end
+
+   private 
+
+   def create_favorite
+    Favorite.create(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deliver_now
+  end
 end
